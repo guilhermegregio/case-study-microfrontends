0a610a55559d1ecfcc5727abd383c8aee99aba40
Guilherme Mangabeira Gregio <guilherme@3gweb.com.br>
2019-09-03 17:40:21 -0300

    feat: redirect netlify

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ rm -rf ./build
 mkdir -p ./build ./build/header ./build/inspection ./build/processing ./build/simulations
 
 cp -R ./packages/my-loan/build/* ./build
-cp -R ./packages/header/build/* ./build/header 
+#cp -R ./packages/header/build/* ./build/header 
 cp -R ./packages/inspection/build/* ./build/inspection 
 cp -R ./packages/processing/build/* ./build/processing 
 cp -R ./packages/simulations/build/* ./build/simulations 
